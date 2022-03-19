b040180c834dce43aa1c44df3952ce3047712a26
namanbhagat <101823239+namanbhagat@users.noreply.github.com>
2022-03-19 19:33:43 +0530

    Update POINTER.c

--- POINTER.c
+++ POINTER.c
@@ -1,19 +1,11 @@
-#include <stdio.h>  
-void swap(int *, int *);
-int main()  
-{  
-    int x = 10;  
-    int y = 20;
-    printf("siddhant\n");
-    printf("Before swapping the values in main x = %d, y = %d\n",x,y);
-    swap(&x,&y);  
-    printf("After swapping values in main x = %d, y = %d\n",x,y);
-}  
-void swap (int *a, int *b)  
-{  
-    int temp;   
-    temp = *a;  
-    *a=*b;  
-    *b=temp;  
-    printf("After swapping values in function a = %d, b = %d\n",*a,*b);  
-}  
+#include <stdio.h>
+int main()
+{
+  int var = 5;
+  printf("naman\n");  
+  printf("var: %d\n", var);
+
+  // Notice the use of & before var
+  printf("address of var: %p", &var);  
+  return 0;
+}
