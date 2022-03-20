3f7042376d701906db6a53b979beea90f2331fe3
namanbhagat <101823239+namanbhagat@users.noreply.github.com>
2022-03-20 10:31:20 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-#include<string.h>
- 
-void main()
-{
-    char a[20];
+int main() {
+    char c;
+    int lowercase_vowel, uppercase_vowel;
+    printf("naman\n");
+    printf("Enter an alphabet: ");
+    scanf("%c", &c);
 
-    puts("enter the string=");
-    gets(a);
-puts("print reverse string");
-puts(strrev(a));
+    // evaluates to 1 if variable c is a lowercase vowel
+    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 
+    // evaluates to 1 if variable c is a uppercase vowel
+    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
+    // evaluates to 1 (true) if c is a vowel
+    if (lowercase_vowel || uppercase_vowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
 }
