06def65bab0731dc7c582b0bc7cb93c355c210d1
namanbhagat <101823239+namanbhagat@users.noreply.github.com>
2022-03-20 10:38:53 +0530

    Update dynamic.c

--- dynamic.c
+++ dynamic.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-int main()
-{
-    int i,j,lines;
-    char ch='A';
-    printf("siddhant\n");
-    printf("Enter the no. of lines");
-    scanf("%d",&lines);
-    for(i=1;i<=lines;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%c ",ch++);
-        }
-        printf("\n");
+#include <stdlib.h>
+
+int main() {
+  int n;
+  double *data;
+  printf("naman\n");  
+  printf("Enter the total number of elements: ");
+  scanf("%d", &n);
+
+  // Allocating memory for n elements
+  data = (double *)calloc(n, sizeof(double));
+  if (data == NULL) {
+  printf("Error!!! memory not allocated.");
+  exit(0);
+  }
+
+  // Storing numbers entered by the user.
+  for (int i = 0; i < n; ++i) {
+  printf("Enter number%d: ", i + 1);
+  scanf("%lf", data + i);
+  }
+
+  // Finding the largest number
+  for (int i = 1; i < n; ++i) {
+    if (*data < *(data + i)) {
+      *data = *(data + i);
     }
-    return 0;
+  }
+  printf("Largest number = %.2lf", *data);
+
+  free(data);
+
+  return 0;
 }
